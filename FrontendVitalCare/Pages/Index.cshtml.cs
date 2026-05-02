3fb8e52cc009acdf5ab95ad7e0be9cfded0b0e49
Jose <jose.lopez.s@ucb.edu.bo>
2026-05-02 14:13:26 -0400

    fix: correcion en frontend

--- FrontendVitalCare/Pages/Index.cshtml.cs
+++ FrontendVitalCare/Pages/Index.cshtml.cs
@@ -7,31 +7,14 @@ namespace ProyectoArqSoft.Pages
 {
     public class IndexModel : PageModel
     {
-        //private readonly IDashboardFacade _dashboardFacade;
-
-        public string? Usuario { get; set; }
-        public DataTable MedicamentoDataTable { get; set; } = new DataTable();
-
-       // public EstadisticasDTO TotalFarmacia { get; set; } = new EstadisticasDTO();
         public IndexModel()
         {
 
         }
 
-        /*public IndexModel( ) //IDashboardFacade dashboardFacade)
-        
-            //_dashboardFacade = dashboardFacade;
-        }*/
-
         public void OnGet()
         {
-            Usuario = HttpContext.Session.GetString("UserName");
 
-           // DashboardDTO dashboard = _dashboardFacade.ObtenerDashboardCompleto();
-
-         //   TotalFarmacia = dashboard.Estadisticas;
-
-          //  MedicamentoDataTable = dashboard.MedicamentosDestacados;
         }
     }
 }

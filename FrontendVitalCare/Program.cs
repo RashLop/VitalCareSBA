2fbc2d26bdb3c6a61cc9b826cdf0ec350182cdb0
RashLop <lopezmejiarasshid@outlook.com>
2026-05-05 16:50:41 -0400

    feat: inyeccion de dependencia y cliente agregado

--- FrontendVitalCare/Program.cs
+++ FrontendVitalCare/Program.cs
@@ -1,7 +1,18 @@
+using System.Text.Json;
+using FrontendVitalCare.Adaptadores;
+using FrontendVitalCare.Dto.MedicamentoDtos;
+using FrontendVitalCare.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpClient<MedicamentoClient>(cliente =>
+{
+    cliente.BaseAddress = new Uri(builder.Configuration["ApiUrls:Medicamentos"]!);
+    cliente.BaseAddress = new Uri(builder.Configuration["ApiUrls:Clasificacion  "]!);
+});
+builder.Services.AddScoped<IAdapter<JsonElement, MedicamentoDto>, MedicamentoAdapter>();
 
 var app = builder.Build();
 
